6c18a68bf0a9314f0c93ab535610b5b2a162d1aa
reachlinlen <reachlinlen@gmail.com>
2019-12-02 20:58:09 +0800

    responsive for login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -20,24 +20,45 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('lg')]: {
       borderRight: `${theme.spacing(1)}px solid ${Color.border}`,
     },
+    [theme.breakpoints.down('md')]: {
+      border: `${theme.spacing(1)}px solid ${Color.border}`,
+    },
   },
   login: {
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(3),
   },
+  socialMediaLogin: {
+    [theme.breakpoints.down('md')]: {
+      paddingLeft: theme.spacing(3),
+    },
+  },
   or: {
-    paddingTop: theme.spacing(5),
+    [theme.breakpoints.up('lg')]: {
+      paddingTop: theme.spacing(5),
+    },
+    [theme.breakpoints.down('md')]: {
+      paddingTop: theme.spacing(5),
+      paddingLeft: '25%',
+    },
   },
   orText: {
-    borderRadius: '50%',
-    maxWidth: '47%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    background: Color.border,
-    textAlign: 'center',
+    [theme.breakpoints.up('lg')]: {
+      borderRadius: '50%',
+      maxWidth: '47%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      background: Color.border,
+      textAlign: 'center',
+    },
   },
   loginContainer: {
-    paddingLeft: theme.spacing(3),
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: theme.spacing(3),
+    },
+    [theme.breakpoints.down('md')]: {
+      paddingTop: theme.spacing(3),
+    },
   },
 }));
 
@@ -46,8 +67,8 @@ function Home() {
 
   return (
     <>
-      <Grid container spacing={2} className={classes.pageContainer}>
-        <Grid item xs={12} lg={3} className={classes.logo}>
+      <Grid container className={classes.pageContainer}>
+        <Grid item xs={5} lg={3} className={classes.logo}>
           <img src="src/assets/images/O.png" alt="OOLogo" />
         </Grid>
         <Grid item xs={12} lg={8}>
@@ -57,13 +78,20 @@ function Home() {
                 Login
               </Typography>
             </Grid>
-            <Grid item xs={12} lg={5}>
+            <Grid item xs={12} lg={5} className={classes.socialMediaLogin}>
               <Button variant="contained" color="primary" href="www.google.com" startIcon={<Icon><img src={google} alt="googleLogo" /></Icon>}>
                 Sign up with Google
               </Button>
             </Grid>
             <Hidden mdDown>
               <Grid item lg={1} className={classes.or}>
+                <Typography variant="body1" className={classes.orText}>
+                  Or
+                </Typography>
+              </Grid>
+            </Hidden>
+            <Hidden lgUp>
+              <Grid item xs={1} className={classes.or}>
                 <Typography variant="body1" className={classes.orText}>
                   Or
                 </Typography>
